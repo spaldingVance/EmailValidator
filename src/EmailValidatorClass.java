ad088f59685f5bd07c877250414d16341a4a2152
Spalding Vance <sclarkvance@gmail.com>
2020-12-02 19:57:00 -0500

    added boiler plate code

---
+++ src/EmailValidatorClass.java
@@ -0,0 +1,11 @@
+
+public class EmailValidatorClass {
+	static String[] emailIDs = new String[] { "bob@email.com", "sally@email.com", "mike@email.com", "terry@email.net", "sara@email.org" };
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
